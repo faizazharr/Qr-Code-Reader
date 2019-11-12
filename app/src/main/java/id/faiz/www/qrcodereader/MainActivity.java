4777398cf2ed9d772fef2d864931c957e77cf87a
faizazharr <faizazharr@github.com>
2019-11-12 08:10:07 +0700

    tambahan garu

--- app/src/main/java/id/faiz/www/qrcodereader/MainActivity.java
+++ app/src/main/java/id/faiz/www/qrcodereader/MainActivity.java
@@ -1,14 +1,57 @@
 package id.faiz.www.qrcodereader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int ZXING_CAMERA_PERMISSION = 1;
+    Button scannerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scannerButton = findViewById(R.id.scannerbutton);
+
+        scannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveIntent = new Intent(MainActivity.this, SimpleScannerActivity.class);
+                startActivity(moveIntent);
+            }
+        });
+    }
+
+    public void launchCustomViewFinderScannerActivity(View v) {
+        launchActivity(SimpleScannerActivity.class);
+    }
+
+    private void launchActivity(Class<?> clss) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            Class<?> mClss = clss;
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, ZXING_CAMERA_PERMISSION);
+        } else {
+            Intent intent = new Intent(this, clss);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
